84bd430b1b660279bb6d63fda093bc540dd9ae27
Atharva Pimple <atharvapimple30@gmail.com>
2025-07-25 14:42:20 +0530

    First commit

---
+++ src/main/java/com/majorproj/agrorent/AgrorentApplication.java
@@ -0,0 +1,13 @@
+package com.majorproj.agrorent;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AgrorentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AgrorentApplication.class, args);
+	}
+
+}
